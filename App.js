88af09074b969a5d12fc66da642750495285055e
unknown <kekzhihui@gmail.com>
2022-06-07 16:29:13 +0800

    finished database app

--- App.js
+++ App.js
@@ -1,12 +1,91 @@
+import React, { useEffect, useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation-stack";
+import { Entypo } from '@expo/vector-icons';
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("notes.db");
+
+function NotesScreen({ navigation }) {
+  // create state variables for our notes
+  const [notes, setNotes] = useState([
+    {title: "walk the dog", done: false, id: "0"},
+    {title: "water the plants", done: false, id: "1"},
+  ]);
+
+  function addNote() {
+    let newNote = {
+      title: "Sample new note",
+      done: false,
+      id: notes.length.toString(),
+    };
+    setNotes([...notes, newNote]);
+  }
+
+  // This adds the new note button in the header
+  useEffect(() => {
+    console.log("This effect happened!!");
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote}>
+          <Entypo name="new-message"
+            size={24}
+            color="black"
+            style={{ marginRight: 16 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  function renderItem({ item }) {
+    return (
+      <View
+      style={{ padding: 10, borderBottomColor: "#ccc", borderBottomWidth: 1}}
+      >
+        <Text style={{ fontSize: 16 }}>{item.title}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={notes}
+        renderItem={renderItem}
+        style={{ width: "100%"}}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            title: "Todo or Notes App idk",
+            headerTitleStyle:{
+              fontWeight: "bold",
+              fontSize: 24,
+            },
+            headerStyle: {
+              backgroundColor: "orange",
+              height: 120,
+              borderBottomColor: "#999",
+              borderBottomWidth: 2,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
