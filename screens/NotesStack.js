5a4ddcf3d119e20de0d99fe776c810095fe52e24
unknown <kekzhihui@gmail.com>
2022-06-08 22:22:42 +0800

    Finished NotesStack

--- screens/NotesStack.js
+++ screens/NotesStack.js
@@ -3,3 +3,27 @@ import { createStackNavigator } from "@react-navigation/stack";
 import NotesScreen from "./NotesScreen"
 
 const Innerstack = createStackNavigator();
+
+export default function NoteStack() {
+    return (
+        <Innerstack.Navigator>
+            <Innerstack.Screen 
+                name="Notes"
+                component={NotesScreen}
+                options={{ 
+                    headerTitle: "Notes App",
+                    headerTitleStyle: {
+                        fontWeight: "bold",
+                        fontSize: 30
+                    },
+                    headerStyle: {
+                    height: 120,
+                    backgroundColor: "yellow",
+                    borderBottomColor: "#ccc",
+                    borderBottomWidth: 1
+                    }
+                }}
+            />
+        </Innerstack.Navigator>
+    )
+}
