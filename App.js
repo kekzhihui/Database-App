b501128fe71946992e2090dfbe78435d93394a2f
unknown <kekzhihui@gmail.com>
2022-06-08 11:34:40 +0800

    NotesScreen completed

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Entypo } from '@expo/vector-icons';
-import * as SQLite from "expo-sqlite";
 import NotesStack from "./screens/NotesStack";
 import AddScreen from "./screens/AddScreen";
 
-const db = SQLite.openDatabase("notes.db");
+
+const Stack = createStackNavigator();
+
 
 function NotesScreen({ navigation }) {
   // create state variables for our notes
@@ -21,76 +20,21 @@ function NotesScreen({ navigation }) {
     navigation.navigate("Add Note");
   }
 
-  // This adds the new note button in the header
-  useEffect(() => {
-    console.log("This effect happened!!");
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={addNote}>
-          <Entypo name="new-message"
-            size={24}
-            color="black"
-            style={{ marginRight: 16 }}
-          />
-        </TouchableOpacity>
-      ),
-    });
-  });
-
-  function renderItem({ item }) {
-    return (
-      <View
-      style={{ padding: 10, borderBottomColor: "#ccc", borderBottomWidth: 1}}
-      >
-        <Text style={{ fontSize: 16 }}>{item.title}</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={notes}
-        renderItem={renderItem}
-        style={{ width: "100%" }}
-      />
-    </View>
-  );
-}
+  
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator mode="modal" headerMode="none">
         <Stack.Screen
-          name="Notes"
-          component={NotesScreen}
-          options={{
-            title: "Todo or Notes App idk",
-            headerTitleStyle:{
-              fontWeight: "bold",
-              fontSize: 24,
-            },
-            headerStyle: {
-              backgroundColor: "orange",
-              height: 120,
-              borderBottomColor: "#999",
-              borderBottomWidth: 2,
-            },
-          }}
+          name="Notes Stack"
+          component={NotesStack}
+          options={{ headerShown: false }}
         />
+        <Stack.Screen name="Add Note" component={AddScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/NotesScreen.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import { TouchableOpacity, Text, View, FlatList, StyleSheet, Touchable } from "react-native";
+import { Entypo, Ionicons } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("notes.db");
+
+export default function NotesScreen({ navigation, route }) {
+    
+    const [notes, setNotes] = useState([]);
+
+    function refreshNotes() {
+        db.transaction((tx) => {
+            tx.executeSql(
+                "SELECT * FROM notes",
+                null,
+                (txObj, { rows: {_array } }) => setNotes(_array),
+                (txObj, error) => console.log("Error ", error)
+            )
+        })
+    }
+
+    useEffect(() => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                `CREATE TABLE IF NOT EXISTS
+                notes
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title Text,
+                done INT);`
+            );
+        }, null, refreshNotes);
+    }, []);
+
+    function addNote() {
+        navigation.navigate("Add Note");
+    }
+
+    // function to delete notes
+    function deleteNote(id) {
+        db.transaction((tx) => {
+            tx.executeSql(`DELETE FROM notes WHERE id=${id}`)
+        }, null, refreshNotes);
+    }
+
+    useEffect(() => {
+        if (route.params?.text) {
+            db.transaction((tx) => {
+                tx.executeSql("INSERT INTO notes (done, title) VALUES (0, ?)", [route.params.text]);
+            }, null, refreshNotes)
+        }
+    }, [route.params?.text])
+
+    // This adds the new note button in the header
+    useEffect(() => {
+        console.log("This effect happened!!");
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity onPress={addNote}>
+                <Entypo 
+                    name="new-message"
+                    size={24}
+                    color="black"
+                    style={{ marginRight: 16 }}
+                />
+                </TouchableOpacity>
+            ),
+        });
+    });
+
+    function renderItem({ item }) {
+        return (
+            <View
+                style={{ 
+                    padding: 10, 
+                    paddingTop: 20,
+                    paddingBottom: 20,
+                    borderBottomColor: "#ccc", 
+                    borderBottomWidth: 1,
+                    flexDirection: "row",
+                    justifyContent: "space-between" // this makes the trash icon move all the way to the right
+                }}
+            >
+            <Text style={{ fontSize: 16, textAlign: "left" }}>{item.title}</Text>
+            <TouchableOpacity onPress={() => deleteNote(item.id)}>
+                <Ionicons
+                    name="trash"
+                    size={16}
+                    color="#944" />
+            </TouchableOpacity>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={notes}
+                renderItem={renderItem}
+                style={{ width: "100%" }}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ffc",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
